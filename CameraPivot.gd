737cf45f44245fcb951c0aec3137b97f4fe106e4
Thiago Borges <thiagoborges533@gmail.com>
2023-08-31 14:53:41 -0300

    Inital commit

---
+++ CameraPivot.gd
@@ -0,0 +1,26 @@
+extends SpringArm3D
+
+@onready var camera = $Camera3D;
+
+func _ready():
+	# this is necessary, otherwise the camera will move 
+	set_as_top_level(true);
+
+func _process(delta):
+	_camera_movement()
+
+func _camera_movement() -> void:
+	if Input.is_action_pressed("camera_backward") and spring_length < 10:
+		spring_length += 0.1;
+	if Input.is_action_pressed("camera_forward") and spring_length > 2:
+		spring_length -= 0.1;
+	
+	var rotation_y = Input.get_axis("camera_left","camera_right");
+	if rotation_y != 0:
+		rotation_degrees.y += rotation_y;
+		rotation_degrees.y = wrap(rotation_degrees.y, 0.0, 360.0);
+		
+	var rotation_x = Input.get_axis("camera_down","camera_up");
+	if rotation_x != 0:
+		rotation_degrees.x += rotation_x;
+		rotation_degrees.x = clamp(rotation_degrees.x, -45.0, 0.0);
---
+++ collectables_list.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@export var area_width: int = 10;
+@export var area_height: int = 10;
+@export var total_collectables: int = 10;
+
+@onready var piece = preload("res://Collectable.tscn");
+
+func _ready():
+	for collectable_number in total_collectables:
+		var new_collectable = piece.instantiate();
+		new_collectable.position = Vector3(
+			randf_range(0, area_width),
+			1,
+			randf_range(0, area_height)
+		);
+		add_child(new_collectable);
+
+
+func _process(delta):
+	pass
