2101c2dfe8a251d6be4da21ca908615db49dcb04
Thiago Borges <thiago@tfcb93.com>
2024-10-01 14:42:22 -0300

    Organizes the code a little bit

--- Scripts/Ground.gd
+++ Scripts/Ground.gd
@@ -1,4 +1,4 @@
-extends StaticBody3D
+extends StaticBody3D;
 
 var collectableListScene := preload("res://Scenes/Collectables_list.tscn");
 var obstacle_list_instance := preload("res://Scenes/Obstacles_list.tscn");
@@ -47,42 +47,53 @@ func _ready() -> void:
 # 	collectableList = collectableListScene.instantiate();
 # 	collectableList.area = areaSize;
 # 	add_child(collectableList);
-	
+
 func generate_per_area() -> void:
 	var divided_size := floorSize.x / area_division;
 	var remaining_collectables := WorldGlobals.totalCollectables;
-	var max_collectable_per_area := WorldGlobals.totalCollectables / area_division ^ 2;
+	var max_collectable_per_area := WorldGlobals.totalCollectables / area_division ** 2;
 	for x in area_division:
 		for z in area_division:
-			var sub_x := randf_range((x * divided_size) + area_size_shift, ((x + 1) * divided_size) + area_size_shift);
-			var sub_z := randf_range((z * divided_size) + area_size_shift, ((z + 1) * divided_size) + area_size_shift);
-			var obstacle_pos := Vector3(sub_x, 0, sub_z);
-			var new_obstacle := obstacle_instance.instantiate();
-			obstacles_list.add_child(new_obstacle);
-			new_obstacle.position = obstacle_pos;
+			
+			var sub_area_obstacle_pos := create_sub_area_obstacle((x * divided_size) + area_size_shift, ((x + 1) * divided_size) + area_size_shift, (z * divided_size) + area_size_shift, ((z + 1) * divided_size) + area_size_shift);
+			var collectables_for_this_sub_area := max_collectable_per_area if (remaining_collectables - max_collectable_per_area > max_collectable_per_area) else remaining_collectables;
+			create_sub_area_collectables((x * divided_size) + area_size_shift, ((x + 1) * divided_size) + area_size_shift, (z * divided_size) + area_size_shift, ((z + 1) * divided_size) + area_size_shift, sub_area_obstacle_pos, collectables_for_this_sub_area);
+			remaining_collectables -= collectables_for_this_sub_area;
 
-			var remaning_collectables_in_area := max_collectable_per_area if remaining_collectables - max_collectable_per_area > max_collectable_per_area else remaining_collectables;
-			var new_collectable_pos: Array[Vector3] = [];
-			var do_it_again := false;
-			while(remaning_collectables_in_area > 0):
-				var col_x := randf_range((x * divided_size) + area_size_shift, ((x + 1) * divided_size) + area_size_shift);
-				var col_z := randf_range((z * divided_size) + area_size_shift, ((z + 1) * divided_size) + area_size_shift);
-				var collectable_pos := Vector3(col_x, 0, col_z);
-				if (collectable_pos.distance_squared_to(obstacle_pos) < 1.2):
-					continue;
-				for used_pos in new_collectable_pos:
-					if(collectable_pos.distance_squared_to(used_pos) < 1.0):
-						do_it_again = true;
-						break;
-				if do_it_again:
-					do_it_again = false;
-					continue;
-				var new_collectable := collectable_instance.instantiate();
-				collectables_list.add_child(new_collectable);
-				new_collectable.position = collectable_pos;
-				new_collectable_pos.push_back(collectable_pos);
+func create_sub_area_obstacle(area_x_0: float, area_x_1: float, area_z_0: float, area_z_1: float) -> Vector3:
+	var sub_x := randf_range(area_x_0, area_x_1);
+	var sub_z := randf_range(area_z_0, area_z_1);
+	
+	var obstacle_pos := Vector3(sub_x, 0, sub_z);
+	var new_obstacle := obstacle_instance.instantiate();
+	obstacles_list.add_child(new_obstacle);
+	new_obstacle.position = obstacle_pos;
+	return obstacle_pos;
 
-				remaning_collectables_in_area -= 1;
+func create_sub_area_collectables(area_x_0: float, area_x_1: float, area_z_0: float, area_z_1: float, sub_area_obstacle_pos: Vector3, total_area_collectables: int) -> void:
+	var remaning_collectables_in_area := total_area_collectables if (total_area_collectables > 0) else 0;
+	var new_collectable_pos: Array[Vector3] = [];
+	var do_it_again := false;
+	while(remaning_collectables_in_area > 0):
+		var col_x := randf_range(area_x_0, area_x_1);
+		var col_z := randf_range(area_z_0, area_z_1);
+		var collectable_pos := Vector3(col_x, 0, col_z);
+		if (collectable_pos.distance_squared_to(sub_area_obstacle_pos) < 1.2):
+			continue;
+		# I'm not a fan of this, but this is the way I thought on verifying the distance over each already generated collectable position.
+		for used_pos in new_collectable_pos:
+			if(collectable_pos.distance_squared_to(used_pos) < 1.0):
+				do_it_again = true;
+				break;
+		if do_it_again:
+			do_it_again = false;
+			continue;
+		var new_collectable := collectable_instance.instantiate();
+		collectables_list.add_child(new_collectable);
+		new_collectable.position = collectable_pos;
+		new_collectable_pos.push_back(collectable_pos);
+
+		remaning_collectables_in_area -= 1;
 
 
 func create_bounderies() -> void:
