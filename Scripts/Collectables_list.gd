a01c6d45ac3036a022ca6b5f3c53c5c1e0396705
Thiago Borges <thiago@tfcb93.com>
2024-10-02 14:35:40 -0300

    Implements probability of time collectables

--- Scripts/Collectables_list.gd
+++ Scripts/Collectables_list.gd
@@ -5,6 +5,9 @@ extends Node
 @export_range(1.0, 2.0, 0.1) var min_obstacle_distance: float = 1.2;
 @export_range(1.0, 2.0, 0.1) var min_collectable_distance: float = 1.0;
 @export_range(1.0, 2.0, 0.1) var min_player_distance: float = 1.0;
+@export_range(30, 70, 1) var timer_collectable_probability := 0; ## Probability of a timer collectable (add 1 second on your timer) to appear in the field. I decided to close between 30 and 70 since I'm not shure about the probability distribution.
+
+var timer_collectables_generated := 0;
 
 func add_to_collectables_list(area_x_0: float, area_x_1: float, area_z_0: float, area_z_1: float, sub_area_obstacle_pos: Vector3, total_area_collectables: int) -> void:
 	var remaning_collectables_in_area := total_area_collectables if (total_area_collectables > 0) else 0;
@@ -29,6 +32,11 @@ func add_to_collectables_list(area_x_0: float, area_x_1: float, area_z_0: float,
 		var new_collectable := collectable_instance.instantiate();
 		add_child(new_collectable);
 		new_collectable.position = collectable_pos;
+		# I think this is not uniform distribution, but it's easy to implement
+		var time_probability := randi_range(0, 100);
+		if (time_probability <= timer_collectable_probability and timer_collectables_generated < WorldGlobals.game_max_timer_collectables):
+			new_collectable.change_add_time(1);
+			timer_collectables_generated += 1;
 		new_collectable_pos.push_back(collectable_pos);
 
 		remaning_collectables_in_area -= 1;
--- Scripts/WorldGlobals.gd
+++ Scripts/WorldGlobals.gd
@@ -2,6 +2,7 @@ extends Node
 
 const game_initial_time := 30.0;
 const game_initial_total_collectable := 100;
+const game_max_timer_collectables := 30;
 
 var totalCollectables := game_initial_total_collectable;
 var totalCollected := 0;
