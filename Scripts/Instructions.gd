cfafca734067b5baf44e85e5cfc1bdf4ad4b5b24
Thiago Borges <thiago@tfcb93.com>
2024-10-04 21:17:17 -0300

    Corrects the game title; Includes the back focus on the instructions page

--- Scripts/Instructions.gd
+++ Scripts/Instructions.gd
@@ -1,4 +1,9 @@
 extends Node;
 
+@onready var back_button := $CanvasLayer/Button;
+
+func _ready() -> void:
+	back_button.grab_focus();
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Start_screen.tscn");
